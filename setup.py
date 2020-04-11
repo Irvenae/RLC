578706fb4a489ba58e1db87fb421ed7b593d8fea
irvenae <irven.aelbrecht@gmail.com>
2020-04-11 15:48:11 +0200

    :pencil: updated setup info

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='RLC',
-    version='0.3',
+    version='0.4',
     packages=['RLC', 'RLC.move_chess', 'RLC.capture_chess', 'RLC.real_chess'],
-    url='https://github.com/arjangroen/RLC',
+    url='https://github.com/irvenae/RLC',
     license='MIT',
-    author='a.groen',
+    author='a.groen, irvenae',
     author_email='arjanmartengroen@gmail.com',
     description='Collection of reinforcement learning algorithms, all applied to chess or chess related problems'
 )
