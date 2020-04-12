a744c7b21f6faca4c5c26be03c23abaaa37c8fd0
irvenae <irven.aelbrecht@gmail.com>
2020-04-12 19:59:21 +0200

    :ambulance: added hotfix for incompatible keras / tensorflow for conv network

--- RLC/capture_chess/agent.py
+++ RLC/capture_chess/agent.py
@@ -4,6 +4,25 @@ from keras.optimizers import SGD
 import numpy as np
 import keras.backend as K
 
+# start hotfix module 'tensorflow._api.v2.config' has no attribute 'experimental_list_devices'
+import tensorflow as tf
+import keras.backend.tensorflow_backend as tfback
+
+def _get_available_gpus():
+    """Get a list of available gpu devices (formatted as strings).
+
+    # Returns
+        A list of available GPU devices.
+    """
+    #global _LOCAL_DEVICES
+    if tfback._LOCAL_DEVICES is None:
+        devices = tf.config.list_logical_devices()
+        tfback._LOCAL_DEVICES = [x.name for x in devices]
+    return [x for x in tfback._LOCAL_DEVICES if 'device:gpu' in x.lower()]
+
+tfback._get_available_gpus = _get_available_gpus
+# end hotfix module 'tensorflow._api.v2.config' has no attribute 'experimental_list_devices' 
+
 def policy_gradient_loss(Returns):
     def modified_crossentropy(action, action_probs):
         cost = (K.categorical_crossentropy(action, action_probs, from_logits=False, axis=1) * Returns)
